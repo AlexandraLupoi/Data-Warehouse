b72442f20df50df4d16cba82e7b6f8ad1b78bdbd
Alexandra Lupoi <alupoi18@gmail.com>
2022-01-28 22:06:05 +0200

    created page for categories

--- usbwebserver/root/header.php
+++ usbwebserver/root/header.php
@@ -1,10 +1,4 @@
 <html>
-    <head>
-        <title>Menu bar</title>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-     </head>
     <body>
         <div class="menu-bar">
         <nav class="menu">
@@ -32,7 +26,7 @@
              <!--<li><div class="top">
 			        <a href="#" class="menu_icon"><i class="fa fa-bars" aria-hidden="true"></i></a>
 		        </div></li>-->
-                <li class="active" id="homeBtn"><i class="fa fa-home" aria-hidden="true"></i><a href="#">Home</a></li>
+                <li class="active" id="homeBtn"><i class="fa fa-home" aria-hidden="true"></i><a href="index.php">Home</a></li>
                 <li><i class="fa fa-shopping-cart" aria-hidden="true"></i><a href="#">Cart</a></li>
                 <li><button class="btn btn-primary" type="button" name="login" id="login" data-toggle="modal" data-target="#loginModal">Login</button></li>
             </ul>
